f2b117f0cf17f1f0fe3d11c7d35a3ed682ea37fc
ShaneKolkoto <shanekolkoto@gmail.com>
2022-09-08 17:29:57 +0200

    fixed function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,16 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
- * main - prints exactly "and that piece of art is useful"
- * - Dora Korpar, 2015-10-19",
- * followed by a new line, to the standard error.
- * Return: Always 0 (Success)
+ * main - Entry point
+ *
+ * Description: print a quote using write function
+ *    ssize_t write(int fd, const void *buf, size_t count);
+ *
+ * Return: This time we return an error 1.
  */
 int main(void)
 {
-    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+    char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+    write(1, quo, 59);
     return (1);
 }
