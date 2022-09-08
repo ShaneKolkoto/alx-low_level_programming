3ed1d83b42479b69f49aa46def9f18def19b74e6
ShaneKolkoto <shanekolkoto@gmail.com>
2022-09-08 17:06:04 +0200

    work

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,6 @@
 #include <stdio.h>
-
-/**
- * main  - Entry point
- *
- * Return: Always 0 (Success)
- */
-
 int main(void)
 {
-    int i;
-    char c;
-    long int li;
-    long long int lli;
-    float f;
-
-    printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
-    printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
-    printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
-    printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
-    printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-    return (0);
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return 0;
 }
