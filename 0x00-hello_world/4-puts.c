7b21ea936d7e968b185f144acb05f69842f1b66f
ShaneKolkoto <shanekolkoto@gmail.com>
2022-09-08 16:59:35 +0200

    fixed function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
-#include <string.h>
 
 /**
- *main - How to use Puts function
+ * main  - Entry point
  *
- *Return: Always 0 (Success)
- **/
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    char phrase[] = "\"Programming is like building a multilingual puzzle";
+    int i;
+    char c;
+    long int li;
+    long long int lli;
+    float f;
 
-    puts(phrase);
+    printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+    printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+    printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
+    printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
+    printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
     return (0);
 }
