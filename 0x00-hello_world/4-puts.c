561b5c5857b939dc78952fcd5576b71300734673
ShaneKolkoto <shanekolkoto@gmail.com>
2022-09-08 17:07:55 +0200

    added description

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
     char phrase[] = "\"Programming is like building a multilingual puzzle";
