7b74433ef074ba31a1db2c7b379725230ea0bfb9
ShaneKolkoto <shanekolkoto@gmail.com>
2022-09-09 09:49:34 +0200

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
- * main - prints exactly "and that piece of art is useful"
- * - Dora Korpar, 2015-10-19",
- * followed by a new line, to the standard error.
- * Return: Always 0 (Success)
+ * main - Entry point
+ * Return:1
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, ch, sizeof(ch));
 return (1);
 }
