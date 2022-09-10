752edd8ec0b1809e58d7bd5168c41067f7ae313e
ShaneKolkoto <shanekolkoto@gmail.com>
2022-09-10 17:11:28 +0200

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,7 @@
  */
 int main(void)
 {
-    char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(1, quo, 59);
-    return (1);
+char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(1, quo, 59);
+return (1);
 }
